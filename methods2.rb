2562dabfe013ddd5b80ddde9d6f0b4bd890ac6e9
Matt Uhlar <mattuhlar@gmail.com>
2015-09-01 10:10:56 -0600

    asdF

--- methods2.rb
+++ methods2.rb
@@ -5,7 +5,17 @@ module Methods2
 		n % 11 < 2
 	end
 	# TODO - write ice_cream_party?
-	
+	def ice_cream_party (a,b)
+		if (a>=2*b or b>=2*a)
+			"great"
+
+		elsif (a>=5 && b>=5)
+			"good"
+
+		else
+			"bad"
+		end
+	end
 	# TODO - write successful_squirrel_party?
 	
 	# TODO - write ticket
--- methods2_test.rb
+++ methods2_test.rb
@@ -1,9 +1,6 @@
 require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
-require 'minitest/autorun'
-require "minitest/reporters"
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative 'methods2'
 
 class Methods2Test < MiniTest::Test
@@ -27,4 +24,11 @@ class Methods2Test < MiniTest::Test
 	  assert_equal true, @m.elevenish(11)
 	end
   
+  def test_ice_cream_party
+		assert_equal "good", @m.ice_cream_party(6,5)
+	  assert_equal "good", @m.ice_cream_party(5,5)
+	  assert_equal "great", @m.ice_cream_party(33,13)
+	  assert_equal "bad", @m.ice_cream_party(4,3)
+	end
+	
 end
